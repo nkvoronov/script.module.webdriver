cb20dad357cbf45b8e177d7cbc1a518b2d5b5160
Nikolay Voronov <nkvoronov@gmail.com>
2021-01-22 14:31:28 +0200

    Update

--- resources/lib/webdriver_select.py
+++ resources/lib/webdriver_select.py
@@ -7,6 +7,10 @@ import xbmcvfs
 import xbmcaddon
 import xbmcgui
 import traceback
+import urllib
+from urllib.request import Request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
 
 addonid = 'script.module.webdriver'
 logErorr = xbmc.LOGERROR
@@ -81,9 +85,63 @@ class WDS(object):
     def getListDriver(self):
         try:
             self.addLog('WDS::getListDriver', 'enter_function')
-            dialog = xbmcgui.Dialog()
-            dialog.ok('SELECT', 'SELECT')
+            xbmc.executebuiltin('ActivateWindow(busydialognocancel)')
+            ListDriver = {}
+            html = self.loadUrl(mhost)
+            soup = bs(html, 'html.parser')
+            self.addLog('WDS::getListDriver', 'soup')
+            if soup:
+                for item in soup.find_all('a', href=True):
+                    if item:
+                        url = str(item['href'])
+                        title = str(item.text)
+                        if title.startswith('ChromeDriver ') and url.startswith(dhost + '?' + dparam + '='):
+                            title = title.split(' ')[1]
+                            if title not in ListDriver:
+                                ListDriver[title] = url
+                                self.addLog('WDS::getListDriver','Data: ' + title + ' -> ' + ListDriver[title])
+            if len(ListDriver) > 0:
+                sel_index = 0
+                sel_source_name = 'none'
+                dialog = xbmcgui.Dialog()
+                sel_index = dialog.select(self.getLang(32002), sorted(ListDriver.keys(), reverse=True))
+                self.addLog('WDS::getListDriver','SLECT URL INDEX: ' + str(sel_index))
+                if sel_index >= 0:
+                    sel_source_name = sorted(ListDriver.keys(), reverse=True)[sel_index]
+                if sel_source_name != 'none':
+                    sel_source_url = ListDriver[sel_source_name]
+                    self.addLog('WDS::getListDriver','SEL URL: ' + sel_source_name + ' : ' + sel_source_url)
+                    #self.downloadDriver(sel_source_url)
+                    self.downloadDriver(sel_source_name)
+            xbmc.executebuiltin('Dialog.Close(busydialognocancel)')
             self.addLog('WDS::getListDriver', 'exit_function')
         except Exception as e:
+            xbmc.executebuiltin('Dialog.Close(busydialognocancel)')
+            dialog = xbmcgui.Dialog()
+            ok = dialog.ok('List Driver', 'ERROR: ' + repr(e))
             self.addLog('WDS::getListDriver', 'ERROR: (' + repr(e) + ')', logErorr)
+
+    def downloadDriver(self, url):
+        try:
+            self.addLog('WDS::downloadDriver', 'enter_function')
+            dialog = xbmcgui.Dialog()
+            dialog.ok(self.getLang(32002), url)
+            self.addLog('WDS::downloadDriver', 'exit_function')
+        except Exception as e:
+            self.addLog('WDS::downloadDriver', 'ERROR: (' + repr(e) + ')', logErorr)
+
+    def loadUrl(self, url):
+        try:
+            self.addLog('WDS::loadUrl', 'enter_function')
+            self.addLog('WDS::loadUrl','OPEN URL: ' + url)
+            headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3',
+            'Content-Type': 'application/x-www-form-urlencoded'}
+            connect = urlopen(Request(url, headers = headers))
+            html = connect.read()
+            connect.close()
+            self.addLog('WDS::loadUrl', 'exit_function')
+            return html.strip().decode('utf-8')
+        except Exception as e:
+            self.addLog('WDS::loadUrl(' + url + ')', 'ERROR: (' + repr(e) + ')', logErorr)
             
